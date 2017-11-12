7a91314dbfaa667f463592db36ec8b473d345ba1
saburday <yteslenko@yahoo.com>
2017-11-12 23:54:22 +0200

    pakemon_reg_res

--- src/index.js
+++ src/index.js
@@ -10,15 +10,42 @@
 export Greeting from './Greeting.js';
 */
 import express from 'express';
+import fetch from 'isomorphic-fetch';
 import canonize from './canonize';
 
 const app = express();
-app.get('/', (reg, res) => {
+app.get('/canonize', (reg, res) => {
    // console.log(reg.query);
   const username = canonize(reg.query.url);
   return res.json({
     url: reg.query.url,
     username,
+  });
+  //  res.send('Hello World')
+});
+
+const baseUrl = 'http://pokeapi.co/api/v2/';
+app.get('/', async (reg, res) => {
+
+  const response = await fetch(`${baseUrl}pokemon`)
+  //console.log(response);
+  const pokemons = await response.json();
+
+    // .then(function(response) {
+    //     if (response.status >= 400) {
+    //         throw new Error("Bad response from server");
+    //     }
+    //     return response.json();
+    // })
+    // .then(function(stories) {
+    //     console.log(stories);
+    // });
+
+   // console.log(reg.query);
+
+  return res.json({
+    qwe: 123,
+    pokemons,
   });
   //  res.send('Hello World')
 });
